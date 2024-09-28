0a050a210dad17933483a394d21cd256bd086cf6
sarah <sarah.buisson@gmail.com>
2024-09-28 16:26:43 +0200

    svg scan

--- src/extractor/SvgExtractor.tsx
+++ src/extractor/SvgExtractor.tsx
@@ -1,36 +1,41 @@
 import { ImportImageFromFile, ImportImageFromURL, TakePhoto } from './Uploaders';
 
 import TogglableWrapper from '../components/TogglableWrapper';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { fileToImageData } from '../service/processImage';
 import { ImageTracer } from '@image-tracer-ts/core';
 
 export function SvgExtractor() {
-
+    const canvasRef = useRef(null);
     const [image, setImage] = useState<File>()
     const [svg, setSvg] = useState("<svg>nothing</svg>")
 
     function onHandleImage(image: File) {
-        console.log("image",image)
+        console.log("image", image)
         setImage(image);
     }
 
     function onProcessImage() {
 
-        fileToImageData(image!,150, 200).then((imageData)=>{
+        fileToImageData(image!, 150, 200).then((imageData) => {
 
-                  const tracer =   new ImageTracer({numberOfColors: 3, lineFilter: true})
+            const canvas: any = canvasRef.current;
+            canvas.width = 512;
+            canvas.height = 256;
+            const context = canvas.getContext("2d");
+            context.putImageData(imageData, 0, 0)
+            const tracer = new ImageTracer()
 
-           const svgstr= tracer.traceImage(
-                          imageData
+            const svgstr = tracer.traceImage(
+                imageData
 
-                        // 'posterized2'
+                // 'posterized2'
 
-                    );
-                  setSvg(svgstr)
+            );
+            setSvg(svgstr)
 
 
-            });
+        });
     }
 
 
@@ -49,6 +54,7 @@ export function SvgExtractor() {
 
 
             <button onClick={onProcessImage}>process image</button>
+            <canvas ref={canvasRef}/>
             {svg && <div dangerouslySetInnerHTML={{__html: svg}}/>}
         </div>
     )
