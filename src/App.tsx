58c05dab9ad05aa8db25d05ca0a65a13421b9f55
sarah <sarah.buisson@gmail.com>
2024-10-12 15:15:56 +0200

    add github auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import './App.css'
 import { SvgExtractor } from './extractor/SvgExtractor.tsx';
+import { GitHubLoginButton } from './save/GitHubLoginButton.tsx';
+
+const onSuccess = response => {
+    console.log(response);
+};
+const onFailure = response => {
+    console.error(response);
+};
+
+
 
 function App() {
     return (
         <>
+       <GitHubLoginButton/>
             <SvgExtractor/>
         </>
     )
---
+++ src/save/GitHubLoginButton.tsx
@@ -0,0 +1,9 @@
+export function GitHubLoginButton() {
+    const loginWithGitHub = () => {
+        const clientID = 'Iv23liZzMuid1Me3knW8';
+        const redirectURI = 'https://sarahbuisson.github.io/content-generator2/';
+        window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientID}&redirect_uri=${redirectURI}`;
+    };
+
+    return <button onClick={loginWithGitHub}>Login with GitHub</button>;
+}
